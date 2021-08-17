94ea6a98b8ab24ab8ca6308c27ff0e0049afe8a3
josan <josanescod@gmail.com>
2021-08-17 17:38:54 +0200

    createGenArrays initGenArrays

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-
 const rows = 40;
 const cols = 40;
+
+let currentGen = [rows];
+let nextGen = [rows];
+
+const createGenArrays = () => {
+    for (let i = 0; i < rows; i++) {
+        currentGen[i] = new Array(cols);
+        nextGen[i] = new Array(cols);
+    }
+    console.log('createGenArrays');
+}
+
+const initGenArrays = () => {
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            currentGen[i][j] = 0;
+            nextGen[i][j] = 0;
+        }
+    }
+    console.log('initGenArrays');
+}
 
 const createWorld = () => {
     let world = document.querySelector('#world');
@@ -20,14 +40,23 @@ const createWorld = () => {
     world.appendChild(table);
 }
 
-const cellClick = () => {
+function cellClick() {
     let location = this.id.split('_');
     let row = Number(location[0]);//Get i
     let col = Number(location[1]);//Get j
-    console.log(row, col);
-    this.className === 'alive' ? this.setAttribute('class', 'dead') : this.setAttribute('class', 'alive');
+    this.className === 'alive'
+        ? (
+            this.setAttribute('class', 'dead'),
+            currentGen[row][col] = 0,
+            console.log(row, col, currentGen[row][col]))
+        : (
+            this.setAttribute('class', 'alive'),
+            currentGen[row][col] = 1,
+            console.log(row, col, currentGen[row][col]))
 }
 
 window.onload = () => {
-    createWorld;
+    createWorld();//the visual table
+    createGenArrays();//current and next generations
+    initGenArrays();//set all array locations to 0=dead
 }
