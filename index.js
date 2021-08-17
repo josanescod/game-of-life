54065ae2f9b94481dda7a50b9b8222127eb611b1
josan <josanescod@gmail.com>
2021-08-17 15:28:57 +0200

    initial configuration

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
 
-console.log('loading script...')
-
 const rows = 40;
 const cols = 40;
 
-function createWorld() {
+const createWorld = () => {
     let world = document.querySelector('#world');
-
     let table = document.createElement('table');
     table.setAttribute('id', 'worldgrid');
-
-
     for (let i = 0; i < cols; i++) {
         let tableRow = document.createElement('tr');
         for (let j = 0; j < cols; j++) {
             let cell = document.createElement('td');
+            cell.setAttribute('id', i + '_' + j);
+            cell.setAttribute('class', 'dead');
+            cell.addEventListener('click', cellClick);
             tableRow.appendChild(cell);
         }
         table.appendChild(tableRow);
-
     }
     world.appendChild(table);
 }
 
+const cellClick = () => {
+    let location = this.id.split('_');
+    let row = Number(location[0]);//Get i
+    let col = Number(location[1]);//Get j
+    console.log(row, col);
+    this.className === 'alive' ? this.setAttribute('class', 'dead') : this.setAttribute('class', 'alive');
+}
+
 window.onload = () => {
-    createWorld();
+    createWorld;
 }
